165c6515023592eeb4482545767f15d4482bd4ab
tqx94 <tangqingxia@gmail.com>
2019-07-14 00:22:25 +0800

    Update loadingofdata.R

--- loadingofdata.R
+++ loadingofdata.R
@@ -11,7 +11,7 @@ packages <- c("dplyr",
 "wordcloud",
 "RColorBrewer",
 "ggplot2",
-"lubridate","reshape2")
+"lubridate","reshape2","textmineR")
 
 # if need to install packages
 for (i in 2:length(packages))
@@ -22,33 +22,18 @@ lapply(packages, require, character.only = TRUE)
 
 
 # Loading data --------------------------------------------------------
-report_reason <- "load your data here"
+#https://www.kaggle.com/crowdflower/first-gop-debate-twitter-sentiment
+data <- fread("~/Documents/Projects/TextAnalytics_LDA/Sentiment.csv")
+#loonking at top 5000 rows
+data <- data %>% select(text,id) %>% head(5000)
 
-# Cleaning all the tokens for future mapping --------------------------------------------------
-# Dictionary for cleaning words
-text_cleaning <- report_reason %>% filter(New_tag != "OOS" ) %>% 
-  filter(is.na(New_tag) == 0) %>% select(Reasons)
-text_cleaning_tokens <- text_cleaning %>% 
-  tidytext::unnest_tokens(word, Reasons) 
+#text cleaning
+data$text <- sub("RT.*:", "", data$text)
+data$text <- sub("@.* ", "", data$text)
+text_cleaning_tokens <- data %>% 
+  tidytext::unnest_tokens(word, text)
 text_cleaning_tokens$word <- gsub('[[:digit:]]+', '', text_cleaning_tokens$word)
 text_cleaning_tokens$word <- gsub('[[:punct:]]+', '', text_cleaning_tokens$word)
 text_cleaning_tokens <- text_cleaning_tokens %>% filter(!(nchar(word) == 1))%>% 
   anti_join(stop_words)
-text_cleaning_tokens <- text_cleaning_tokens %>% filter(!(word==""))
-#count the dirty tokens
-text_cleaning_tokens_count <- text_cleaning_tokens %>% count(word)
-
-
-#map the cleaned ones in
-final_dic <- fread("~/text_cleaning_tokens_countV3.csv",header = T)
-final_dic <- final_dic %>% group_by(word) %>% distinct(replacement)
-cleaned_tokens <- left_join(text_cleaning_tokens, final_dic)
-cleaned_tokens <- cleaned_tokens %>% 
-  mutate(replacement = ifelse(is.na(replacement)==1, word, replacement))
-#count the cleaned tokens
-cleaned_tokens_count <- cleaned_tokens %>% count(replacement)
-cleaned_tokens <- cleaned_tokens %>% group_by(word) %>% distinct(replacement) %>% ungroup()
-#mapped!! :)
-saveRDS(cleaned_tokens, "cleaned_tokens.rds")
-cleaned_tokens <- readRDS("~/topicmodelling/cleaned_tokens.rds")
-
+tokens <- text_cleaning_tokens %>% filter(!(word==""))
